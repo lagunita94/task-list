2abe47c9d25d02ecd44bb14cc868f8a0b38da98e
lagunita94 <barick.abhijeet@gmail.com>
2019-01-16 15:31:50 +0530

    added materialize file

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ function clearTasks() {
 function filterTasks(e) {
     const text = e.target.value.toLowerCase();
     document.querySelectorAll('.collection-item').forEach(function(task){
-        const item = task.firstChild.textContent;
+        const item = task.textContent;
         if(item.toLowerCase().indexOf(text) != -1){
             task.style.display = 'block';
         } else {
